c6d98f3e81110d59096a741c2debb2c6cf9df474
byCRXHIT <70042525+byCRXHIT@users.noreply.github.com>
2021-09-13 21:24:35 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,33 @@
+const aoijs = require("aoi.js")
+
+const bot = new aoijs.Bot({
+    token: "TOKEN", 
+    prefix: "PREFIX"
+})
+
+bot.status({
+    text: "*play <song>",
+    type: "LISTENING"
+  })
+
+bot.musicEndCommand({
+    channel: "$channelID",
+    code: `$title[Stoppe Song]
+$description[Nothing to play in queue more. Leaving Channel.]
+$color[RED]`
+})
+
+bot.musicStartCommand({
+    channel: "$channelID",
+    code: `$title[Spiele Song]
+    $description[Playing now: **$songInfo[title]**
+Lenght: **$songInfo[duration] Minuten**
+Publisher: **$songInfo[publisher]**]
+$footer[Bot made by: byCRXHIT]
+    $color[GREEN]
+    $thumbnail[$songInfo[thumbnail]]
+    $clear[1]`
+})
+
+bot.onMessage()
+bot.loadCommands(`./commands/`)
