a6eefd25f5d26d55a20718abe3629b4803354a17
byCRXHIT <70042525+byCRXHIT@users.noreply.github.com>
2021-09-13 21:40:18 +0200

    Add files via upload

---
+++ commands/admin/eval.js
@@ -0,0 +1,5 @@
+module.exports = [{
+    name: 'eval',
+    code: `$eval[$message]
+    $onlyForIDs[758444849212555296;No.]`
+}]
---
+++ commands/admin/status.js
@@ -0,0 +1,5 @@
+module.exports = [{
+    name: 'debug',
+    code: `ServerCount: $serverCount
+    $onlyForIDs[758444849212555296;No.]`
+}]
